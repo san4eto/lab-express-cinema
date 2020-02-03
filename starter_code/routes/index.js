dab91606d24e2dcbbad20c48c64fe6b2315f988c
san4eto <59312005+san4eto@users.noreply.github.com>
2020-02-03 20:54:14 +0100

    restart

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,15 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  // res.send();
+  res.render("index.hbs");
+});
+
+router.get("/movies", (req, res, next) => {
+  res.render("movies.hbs");
 });
 
 module.exports = router;
